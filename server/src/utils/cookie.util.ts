661173135e55250f8e895c41f0581e904b55fa7a
Aslah7070 <aslah.c7070@gmail.com>
2025-10-08 15:58:19 +0530

    issue on my auth on deployment

--- server/src/utils/cookie.util.ts
+++ server/src/utils/cookie.util.ts
@@ -2,20 +2,20 @@ import { env } from "../configs/env.configs";
 import { Response } from "express";
 
 export function setCookie(res: Response, refreshToken: string) {
-console.log("cookie secure",env.NODE_ENV==="PRODUCTION")
-console.log("cookie ",env.NODE_ENV=="PRODUCTION")
+console.log("cookie secure",env.NODE_ENV==="production")
+console.log("cookie ",env.NODE_ENV=="production")
     res.cookie("refreshToken", refreshToken, {
        httpOnly: true,
-            secure: env.NODE_ENV==="PRODUCTION",
+            secure: env.NODE_ENV==="production",
             maxAge: 7 * 24 * 60 * 60 * 1000,
-            sameSite:env.NODE_ENV === "PRODUCTION" ? "none" : "lax",
+            sameSite:env.NODE_ENV === "production" ? "none" : "lax",
     });
 }
 
 export function deleteCookie(res: Response) { 
     res.clearCookie('refreshToken', {
         httpOnly: true,
-        secure: process.env.NODE_ENV === 'PRODUCTION',
-        sameSite: env.NODE_ENV === 'PRODUCTION' ? 'none' : 'strict'
+        secure: process.env.NODE_ENV === 'production',
+        sameSite: env.NODE_ENV === 'production' ? 'none' : 'strict'
     })
 }
